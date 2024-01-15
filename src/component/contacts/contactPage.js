5ee29ae4443ef149696a43a726fdaf8aaa8f147a
AyushW07 <ayushwasnik999@gmail.com>
2024-01-15 14:54:49 +0530

    enquiry modified

--- src/component/contacts/contactPage.js
+++ src/component/contacts/contactPage.js
@@ -106,7 +106,7 @@ const ContactPage = () => {
   };
 
   return (
-    <div className="bg-[#F1F5F9] bg-cover h-[100vh]">
+    <div className="bg-[#F1F5F9] bg-cover h-[100vh] ml-[8rem]">
       <div className="flex flex-col">
         <div className="flex flex-row">
           <h1
